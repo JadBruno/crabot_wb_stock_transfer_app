31cc951b42254b4bf49984ded8ac9f7e63858fd4
Eugene Severin <zhokaogurca@gmail.com>
2025-08-30 22:55:59 +0700

    removed wb api_key

--- dependencies/dependencies.py
+++ dependencies/dependencies.py
@@ -16,7 +16,6 @@ class Dependencies:
         self._mysql_controller = None
         self._cookie_jar = None
         self._authorized_headers = None
-        self._wb_content_api_key = None
 
 
     @property
@@ -59,10 +58,6 @@ class Dependencies:
         # headers_copy['AuthorizeV3'] = self.cookie_utils.extract_tokenV3(decrypted_cookies)
         return headers_copy
     
-    @cached_property
-    def wb_content_api_key(self):
-        wb_content_api_key = self.access_data_loader.get_wb_content_api_key()
-        return wb_content_api_key
 
 
 deps = Dependencies()
@@ -70,6 +65,5 @@ deps = Dependencies()
 logger = deps.logger
 cookie_jar = deps.cookie_jar
 authorized_headers = deps.authorized_headers
-wb_content_api_key = deps.wb_content_api_key
 mysql_controller = deps.mysql_controller
 api_controller = SyncAPIController()
