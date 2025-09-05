7b9d947b84d276684276d4cc8c5b4f25addee970
Eugene Severin <zhokaogurca@gmail.com>
2025-09-06 02:06:09 +0700

    ddfgfdsg

--- main_stock_transfer.py
+++ main_stock_transfer.py
@@ -57,9 +57,10 @@ def main():
 
         now = datetime.now()
 
-        if now.minute != 0:
+        if now.minute != 0 and now.second == 1:
                 next_hour = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
                 wait_seconds = (next_hour - now).total_seconds()
+                wait_seconds += 1 # Добавляем 1 секунду для надежности
                 logger.info(f"Ждём до {next_hour.strftime('%H:%M:%S')} ({int(wait_seconds)} сек.)")
                 time.sleep(wait_seconds)
                 
